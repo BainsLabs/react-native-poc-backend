9d253a229a5915c9d35fa1869cb5af8869d2eb1b
Karan Kumar <karan.kumar@bainslabs.com>
2019-04-09 17:17:34 +0530

    face-recog api

--- faceapp/settings.py
+++ faceapp/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third party apps
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- facerecognition/views.py
+++ facerecognition/views.py
@@ -4,47 +4,50 @@ import face_recognition
 import cv2
 import numpy as np
 from PIL import Image
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
 
 
+@api_view(["GET", "POST"])
 def index(request):
+    try:
+        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        MEDIA_ROOT = os.path.join(BASE_DIR, 'facerecognition')
+        # images = os.listdir('')
+        # make a list of all the available images
+        images = MEDIA_ROOT + "/karan.jpg"
 
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'facerecognition')
-    # images = os.listdir('')
-    # make a list of all the available images
-    images = MEDIA_ROOT + "/karan.jpg"
+        cam = cv2.VideoCapture(0)   # 0 -> index of camera
+        s, img = cam.read()
+        if s:    # frame captured without any errors
+            cv2.namedWindow("cam-test")
+            cv2.imshow("cam-test", img)
+            # cv2.waitKey(0)
+            cv2.destroyWindow("cam-test")
+            cv2.imwrite("filename.jpg", img)
 
-    cam = cv2.VideoCapture(0)   # 0 -> index of camera
-    s, img = cam.read()
-    if s:    # frame captured without any errors
-        cv2.namedWindow("cam-test")
-        cv2.imshow("cam-test", img)
-        # cv2.waitKey(0)
-        cv2.destroyWindow("cam-test")
-        cv2.imwrite("filename.jpg", img)
+        # load your image
+        image_to_be_matched = face_recognition.load_image_file("filename.jpg")
 
-    # load your image
-    image_to_be_matched = face_recognition.load_image_file("filename.jpg")
+        # encoded the loaded image into a feature vector
+        image_to_be_matched_encoded = face_recognition.face_encodings(
+            image_to_be_matched)[0]
 
-    # encoded the loaded image into a feature vector
-    image_to_be_matched_encoded = face_recognition.face_encodings(
-        image_to_be_matched)[0]
+        current_image = face_recognition.load_image_file(images)
+        # encode the loaded image into a feature vector
+        current_image_encoded = face_recognition.face_encodings(current_image)[
+            0]
+    # match your image with the image and check if it matches
+        result = face_recognition.compare_faces(
+            [image_to_be_matched_encoded], current_image_encoded)
+    # check if it was a match
+        if result[0] == True:
+            return Response(data={"status": 200})
+        else:
+            return Response(data={"status": 400})
 
-    current_image = face_recognition.load_image_file(images)
-    # encode the loaded image into a feature vector
-    current_image_encoded = face_recognition.face_encodings(current_image)[
-        0]
-  # match your image with the image and check if it matches
-    result = face_recognition.compare_faces(
-        [image_to_be_matched_encoded], current_image_encoded)
-   # check if it was a match
-    if result[0] == True:
-        print("Matched: " + images)
-    else:
-        print("Not matched: " + images)
-
-    # iterate over each image
-    # for image in images:
-    #     # load the image
+    except Exception as e:
+        return Response(data={"message": "not found"}, status=status.HTTP_404_NOT_FOUND)
