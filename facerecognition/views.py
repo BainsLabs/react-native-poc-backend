461c53219fbc1e35d4ef171e316dc1537657f60a
Akshay Sharma <akshay.sharma@bainslabs.com>
2019-04-17 15:30:34 +0530

    face recoginition api changes

--- facerecognition/views.py
+++ facerecognition/views.py
@@ -22,47 +22,47 @@ import base64
 @api_view(["POST"])
 def index(request):
     try:
-        user = User.objects.get(official_email_id=request.data['email'])
+        users = User.objects.all()
+        for user in users:
+            BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+            MEDIA_ROOT = os.path.join(BASE_DIR, 'facerecognition')
+            # images = os.listdir('')
+            # make a list of all the available images
 
-        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        MEDIA_ROOT = os.path.join(BASE_DIR, 'facerecognition')
-        # images = os.listdir('')
-        # make a list of all the available images
+            images = skimage.io.imread(user.image_url)
+            # cam = cv2.VideoCapture(0)   # 0 -> index of camera
+            # s, img = cam.read()
+            # if s:    # frame captured without any errors
+            #     cv2.namedWindow("cam-test")
+            #     cv2.imshow("cam-test", img)
+            #     # cv2.waitKey(0)
+            #     cv2.destroyWindow("cam-test")
+            #     cv2.imwrite("filename.jpg", img)
 
-        images = skimage.io.imread(user.image_url)
-        # cam = cv2.VideoCapture(0)   # 0 -> index of camera
-        # s, img = cam.read()
-        # if s:    # frame captured without any errors
-        #     cv2.namedWindow("cam-test")
-        #     cv2.imshow("cam-test", img)
-        #     # cv2.waitKey(0)
-        #     cv2.destroyWindow("cam-test")
-        #     cv2.imwrite("filename.jpg", img)
+            # load your image
+            image = face_recognition.load_image_file(io.BytesIO(base64.b64decode(request.data["img_base"])))
 
-        # load your image
-        image = face_recognition.load_image_file(io.BytesIO(base64.b64decode(request.data["img_base"])))
+            # image = face_recognition.load_image_file("filename.jpg")
 
-        # image = face_recognition.load_image_file("filename.jpg")
+            # encoded the loaded image into a feature vector
+            image_to_be_matched_encoded = face_recognition.face_encodings(image)[0]
 
-        # encoded the loaded image into a feature vector
-        image_to_be_matched_encoded = face_recognition.face_encodings(image)[0]
+            current_image = images
+            # encode the loaded image into a feature vector
+            current_image_encoded = face_recognition.face_encodings(current_image)[
+                0]
+        # match your image with the image and check if it matches
+            # print("texst1")
+            result = face_recognition.compare_faces(
+                [image_to_be_matched_encoded], current_image_encoded)
+        # check if it was a match
+            # print("texst2", result)
 
-        current_image = images
-        # encode the loaded image into a feature vector
-        current_image_encoded = face_recognition.face_encodings(current_image)[
-            0]
-    # match your image with the image and check if it matches
-        # print("texst1")
-        result = face_recognition.compare_faces(
-            [image_to_be_matched_encoded], current_image_encoded)
-    # check if it was a match
-        # print("texst2", result)
-        emplpoyee = EmployeeDetails.objects.get(official_email=request.data['email'])
-
-        if result[0]:
-            return Response(data={"status": 200,"employee_profile":emplpoyee.to_json,"user":user.to_json},status=status.HTTP_200_OK)
-        else:
-            return Response(data={"status": 400,"message":"Not Matched"},status=status.HTTP_400_BAD_REQUEST)
+            if result[0]:
+                emplpoyee = EmployeeDetails.objects.get(official_email=user.official_email_id)
+                return Response(data={"status": 200,"employee_profile":emplpoyee.to_json,"user":user.to_json},status=status.HTTP_200_OK)
+            # else:
+            #     return Response(data={"status": 400,"message":"Not Matched"},status=status.HTTP_400_BAD_REQUEST)
 
 
 
@@ -87,7 +87,7 @@ def employeDetail(request):
         user= User.objects.get(official_email_id=request.data['email'])
         if user.is_superuser:
           emplpoyee = EmployeeDetails.objects.get(official_email=request.data['email'])
-            return Response(data={"employee":emplpoyee.to_json,"user":user.to_json}, status=status.HTTP_200_OK)
+          return Response(data={"employee":emplpoyee.to_json,"user":user.to_json}, status=status.HTTP_200_OK)
         else:
             return Response(data={"message":"You are not Authorized for this view"}, status=status.HTTP_401_UNAUTHORIZED)
     except Exception as e:
